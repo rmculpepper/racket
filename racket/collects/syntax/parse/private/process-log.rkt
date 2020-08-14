41b14f4e46b993b733287cb211188ded526b4a58
Ryan Culpepper <ryanc@racket-lang.org>
2020-08-14 16:58:53 +0200

    ... !! process-log

--- racket/collects/syntax/parse/private/process-log.rkt
+++ racket/collects/syntax/parse/private/process-log.rkt
@@ -13,86 +13,109 @@
 (define (I x) (if x 1 0))
 (define (hinc! h k e) (hash-set! h k (+ e (hash-ref h k 0))))
 
-;; ClauseSets info
-(define cs-count 0)
-(define cs-npatternss null)
-(define cs-flagh (make-hasheq))
-
-;; Pattern info
-(define p-count 0)
-(define m-sizes null)
-(define w-sizes null)
-(define m-flagh (make-hasheq))
-(define w-flagh (make-hasheq))
-
 ;; ============================================================
 
 (define known-flags
   '(E-cut E-multi cut undo expr or head lit s-class h-class reflect inlsc fail dots
           dots-alts dots-head dots-repc dots-nnil))
 
-(define (process-cs-info info)
-  ;; info : (Listof (list Pattern Props MainPattern Props))
-  (inc! cs-count 1)
-  (push! cs-npatternss (length info))
-  (define allprops (remove-duplicates (apply append (map cadr info))))
-  (for ([flag (in-list known-flags)])
-    (hinc! cs-flagh flag (I (memq flag allprops))))
-  (for ([pinfo (in-list info)])
-    (process-p-info pinfo)))
+(define collector%
+  (class object%
+    ;; ClauseSets info
+    (define cs-count 0)
+    (define cs-npatternss null)
+    (define cs-flagh (make-hasheq))
 
-(define (process-p-info info)
-  (inc! p-count 1)
-  (match info
-    [(list pattern props main-pattern main-props)
-     (push! w-sizes (pattern-size pattern))
-     (push! m-sizes (pattern-size main-pattern))
-     (for ([flag (in-list known-flags)])
-       (hinc! w-flagh flag (I (memq flag props)))
-       (hinc! m-flagh flag (I (memq flag main-props))))]))
+    ;; Pattern info
+    (define p-count 0)
+    (define m-sizes null)
+    (define w-sizes null)
+    (define m-flagh (make-hasheq))
+    (define w-flagh (make-hasheq))
 
-(define (process-simple p-sexpr) (void))
+    (super-new)
+
+
+    (define/public (process-cs-info info)
+      ;; info : (Listof (list Pattern Props MainPattern Props))
+      (inc! cs-count 1)
+      (push! cs-npatternss (length info))
+      (define allprops (remove-duplicates (apply append (map cadr info))))
+      (for ([flag (in-list known-flags)])
+        (hinc! cs-flagh flag (I (memq flag allprops))))
+      (for ([pinfo (in-list info)])
+        (process-p-info pinfo)))
+
+    (define/public (process-p-info info)
+      (inc! p-count 1)
+      (match info
+        [(list pattern props main-pattern main-props)
+         (push! w-sizes (pattern-size pattern))
+         (push! m-sizes (pattern-size main-pattern))
+         (for ([flag (in-list known-flags)])
+           (hinc! w-flagh flag (I (memq flag props)))
+           (hinc! m-flagh flag (I (memq flag main-props))))]))
+
+    (define/public (process-simple p-sexpr) (void))
+
+    ;; ----------------------------------------
+
+    (define/public (print-flag-table flagh total)
+      (printf "Features used:\n")
+      (for ([flag (in-list known-flags)])
+        (define flagct (hash-ref flagh flag 0))
+        (printf " - ~a: ~a (~a%)\n"
+                (~a #:width 12 flag)
+                (~r #:min-width 4 flagct)
+                (~r #:precision 2 (* 100.0 (/ flagct total))))))
+
+    (define/public (print)
+      (printf "== Clause-Set Info ==\n")
+      (printf "Set count: ~s\n" cs-count)
+      (unless (zero? cs-count)
+        (printf "Set mean patterns: ~a\n" (~r #:precision 2 (mean cs-npatternss)))
+        (for ([md (in-list '(1 2))])
+          (let ([ct (for/sum ([n (in-list cs-npatternss)] #:when (= n md)) 1)])
+            (printf " w/ ~a patterns: ~a (~a%)\n" md ct (~r #:precision 2 (* 100 ct (/ cs-count))))))
+        (print-flag-table cs-flagh cs-count))
+      (newline)
+
+      (printf "== Pattern Info ==\n")
+      (printf "Pattern count: ~s\n" p-count)
+      (unless (zero? p-count)
+        (printf "Whole pattern mean size: ~a\n" (~r #:precision 2 (mean w-sizes)))
+        (printf "Whole pattern flags:\n")
+        (print-flag-table w-flagh p-count)
+        (when SHOW-MAIN?
+          (printf "Main pattern mean size: ~a\n" (~r #:precision 2 (mean m-sizes)))
+          (printf "Main pattern flags:\n")
+          (print-flag-table m-flagh p-count))))
+
+    ))
 
 ;; ============================================================
+
+(define pre (new collector%))
+(define post (new collector%))
 
 (for ([e (in-list (parse-log-input (current-input-port)))])
   (case (car e)
     [("simple")             ;; Pattern
-     (process-simple (cdr e))]
+     (send pre process-simple (cdr e))]
     [("patterns")           ;; (Listof (list Pattern Props MainPattern Props))
-     (process-cs-info (cdr e))]
+     (send pre process-cs-info (cdr e))]
     [("post/subpatterns")   ;; (Listof (list Pattern Props MainPattern Props))
-     (void)]
+     (send post process-cs-info (cdr e))]
     [else (void)]))
+
+(printf "** Pre-optimization **\n")
+(send pre print)
+(newline)
+(printf "** Post-optimization **\n")
+(send post print)
 
 ;; ----------------------------------------
 
-(define (print-flag-table flagh total)
-  (printf "Features used:\n")
-  (for ([flag (in-list known-flags)])
-    (define flagct (hash-ref flagh flag 0))
-    (printf " - ~a: ~a (~a%)\n"
-            (~a #:width 12 flag) (~r #:min-width 4 flagct) (~r #:precision 2 (* 100.0 (/ flagct total))))))
-
-(printf "== Clause-Set Info ==\n")
-(printf "Set count: ~s\n" cs-count)
-(unless (zero? cs-count)
-  (printf "Set mean patterns: ~a\n" (~r #:precision 2 (mean cs-npatternss)))
-  (for ([md (in-list '(1 2))])
-    (let ([ct (for/sum ([n (in-list cs-npatternss)] #:when (= n md)) 1)])
-      (printf " w/ ~a patterns: ~a (~a%)\n" md ct (~r #:precision 2 (* 100 ct (/ cs-count))))))
-  (print-flag-table cs-flagh cs-count))
-(newline)
-
-(printf "== Pattern Info ==\n")
-(printf "Pattern count: ~s\n" p-count)
-(unless (zero? p-count)
-  (printf "Whole pattern mean size: ~a\n" (~r #:precision 2 (mean w-sizes)))
-  (printf "Whole pattern flags:\n")
-  (print-flag-table w-flagh p-count)
-  (when SHOW-MAIN?
-    (printf "Main pattern mean size: ~a\n" (~r #:precision 2 (mean m-sizes)))
-    (printf "Main pattern flags:\n")
-    (print-flag-table m-flagh p-count)))
+;; FIXME: post-optimization "clause set" isn't :(
 
 ;; FIXME: dots-head reporting messed up
